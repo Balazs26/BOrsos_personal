6a8c09796bb6463fba387c54fceb84f2f7f335af
Balazs26 <131625311+Balazs26@users.noreply.github.com>
2025-11-06 05:22:46 +0100

    Add files via upload

--- 001_minyon_rfid.ino
+++ 001_minyon_rfid.ino
@@ -1,5 +1,6 @@
 
 #include "EasyMFRC522.h"
+#include <LiquidCrystal.h>
 
 
 #define MAX_STRING_SIZE 100  
@@ -8,6 +9,17 @@
 int openTime = 2000;
 
 EasyMFRC522 rfidReader(53, 5); 
+
+int button = 22;
+
+int LCD_RS = 12;
+int LCD_ENABLE = 11;
+int LCD_D4 = 10;
+int LCD_D5 = 9;
+int LCD_D6 = 8;
+int LCD_D7 = 7;
+
+LiquidCrystal lcd(LCD_RS, LCD_ENABLE, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
 String removeQuotes(String input) {
   String result = "";
@@ -21,23 +33,35 @@ String removeQuotes(String input) {
 
 
 void setup() {
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
   Serial.begin(9600);
   Serial.setTimeout(20000);
   rfidReader.init(); 
 
   pinMode(3, OUTPUT);
+ 
+  pinMode(14, OUTPUT);
+  digitalWrite(14, HIGH);
+
+  pinMode(button, INPUT);
+  
+
+  lcd.begin(16,2);
+  readYourCard();
 }
 
 
 
 void loop() {
 
-
+  
 
   bool success;
   do {
     success = rfidReader.detectTag();    
     delay(50);
+    checkForButton();
   } while (!success);
 
   
@@ -53,7 +77,16 @@ void loop() {
       OPEN();
     } else {
       Serial.println("Belepes megtagadva!");
+      lcd.setCursor(0,0);
+      accessDenied();
+      delay(openTime);
+      lcd.setCursor(0,0);
+      readYourCard();
     }
+  } else if(success) {
+    unknownCard();
+    delay(openTime);
+    readYourCard();
   }
 
 
@@ -61,12 +94,56 @@ void loop() {
   delay(100);
 }
 
+void checkForButton(){
+  int pressed = digitalRead(button);
+
+  if(pressed) {
+
+  }
+}
+
 void OPEN(){
   Serial.println("Belepes engedelyezve!");
+  lcd.setCursor(0,0);
+  accessGranted();
 
   digitalWrite(3, HIGH);
   delay(openTime);
   digitalWrite(3, LOW);
+  lcd.setCursor(0,0);
+  readYourCard();
+}
+
+void readYourCard(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("OLVASD LE A");
+  lcd.setCursor(0, 1);
+  lcd.print("KARTYAD!");
+}
+
+void accessGranted(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("BELEPES");
+  lcd.setCursor(0,1);
+  lcd.print("ENGEDELYEZVE!");
+}
+
+void accessDenied(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("BELEPES");
+  lcd.setCursor(0,1);
+  lcd.print("MEGTAGADVA!");
+}
+
+void unknownCard(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("ISMERETLEN");
+  lcd.setCursor(0,1);
+  lcd.print("KARTYA!");
 }
 
 
